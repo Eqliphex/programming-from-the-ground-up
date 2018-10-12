4ff78d7b21761b61bc67461a0857e6bdac861ecb
Eqliphex <tbt_paddik@hotmail.com>
2018-10-12 13:47:24 +0200

    new commit

--- first-program/exit.s
+++ first-program/exit.s
@@ -16,23 +16,31 @@
 #          %eax holds the system call number
 #          %ebx holds the return status
 #
-.section .data
-.section .text
 
-.global _start
+#    	   Assembler directives:
+.section .data		# Starts the "data" section
 
-#	   Instruction format: mnemonic src dst
+.section .text		# Starts the "text" section, where program
+			# instructions live. 
+
+.globl _start		# Symbol, meaning that it is a placeholder
+			# so programmers can refer to _start
+			# instead of its location address.
+
+#	   Instruction format: [mnemonic] [src] [dst]
 _start:
 movl $1, %eax		# this is the linux kernel command
-			# number (system vall) for exiting
+			# number (system call) for exiting
 			# a program
+			# moves the litteral number 1 into eax
+			# register via immidiate mode addressing ($)
 
-movl $0, %ebx		# this is the linux status number we will
+movl $2, %ebx		# this is the linux status number we will
 			# return to the operating system
 			# Change this around and it will 
 			# return different things to 
 			# echo $?
 
 int $0x80		# this wakes up the kernel to run 
-			# the exit command
+			# the exit command int = interrupt
 
