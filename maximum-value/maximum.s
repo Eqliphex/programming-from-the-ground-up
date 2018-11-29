88e4b13d70bb278a6c0dc037f6dc856d8bf1626c
Eqliphex <tbt_paddik@hotmail.com>
2018-11-29 08:41:52 +0100

    updated maximum-value

--- maximum-value/maximum.s
+++ maximum-value/maximum.s
@@ -1,5 +1,5 @@
 #PURPOSE:	This program finds the maximum number of a
-# 				set of data items.
+# 		set of data items.
 #
 
 #VARIABLES:	The registers have the following uses:
@@ -24,8 +24,8 @@ data_items:	# These are the data items
   .globl _start
  
   _start:
-   	movl $0, %edi										# move 0 into the index register
-   	movl data_items(,%edi,4), %eax	# load the first byte of data
+   		movl $0, %edi									# move 0 into the index register
+   		movl data_items(,%edi,4), %eax							# load the first byte of data
 	 	movl %eax, %ebx									# since this is the first item,
 												# %eax is the biggest.
 
@@ -33,14 +33,14 @@ data_items:	# These are the data items
 	 	cmpl $0, %eax 									# compare current with 0, to see if end
 	 	je loop_exit
 	 	incl %edi									# increments %edi by one
-	 	movl data_items(,%edi,4), %eax	# move pointer 4 bytes since long type 	
+	 	movl data_items(,%edi,4), %eax							# move pointer 4 bytes since long type 	
 		cmpl %ebx, %eax									# compare current item with largest value
 		jle start_loop									# jump to loop beginning if the new
 												# value isn't bigger
 		movl %eax, %ebx									# save the current value as the largest
 		jmp start_loop									# jump to loop beginning
 
-	loop_exit:
+  loop_exit:
 		# %ebx is the status code for the exit system call
 		# and it already has the maximum number
 		movl $1, %eax									# 1 is the exit() syscall
